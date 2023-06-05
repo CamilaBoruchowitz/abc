495c62bc2d38b965136561cd77f48af576eb8188
Camila <camilab@duck.com>
2023-06-05 20:01:38 +0200

    grid

---
+++ clasecsshtml/pages/index.js
@@ -0,0 +1,6 @@
+let password = document.getElementById('password').value;
+ /*document tiene infinidad de funciones o métodos */
+ console.log(password);
+ let otropassword = document.getElementById('otropassword').value;
+ console.log(otropassword)
+ /*me trae ese que elegí busca ese id */
